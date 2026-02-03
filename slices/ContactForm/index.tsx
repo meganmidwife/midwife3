109cbf7409da6a44db1b5adcad4e97dc330ec123
megannidwife <meganthemidwife@outlook.com>
2026-02-03 17:12:40 +0000

    updated  contact page added phone and email options added aligiment variations for movible and desktop  4

--- slices/ContactForm/index.tsx
+++ slices/ContactForm/index.tsx
@@ -66,9 +66,9 @@ const ContactForm = ({ slice }: ContactFormProps): JSX.Element => {
           </div>
         </div>
 
-        <form className="">
+        <form className="m-3">
           <div className="email grid grid-cols-3 align-middle items-center border-b-2 border-gray-500">
-            <label htmlFor="frm-email" className="col-span-3 md:col-span-1  text-center w-full  md:text-left border-2 border-gray-200  bg-gray-800 mt-3 pl-1 md:pl-4 pr-1 md:pr-3 md:border-0 md:bg-logohovercolor">Email</label>
+            <label htmlFor="frm-email" className="col-span-3 md:col-span-1 text-gray-900  text-center font-bold py-2 w-full  md:text-left border-2 border-gray-200  bg-logocolor md:m-3 pl-1 md:pl-4 pr-1 md:pr-3 md:border-0 md:bg-logocolor">Email</label>
             <input 
               type="email" 
               id="frm-email" 
@@ -76,13 +76,13 @@ const ContactForm = ({ slice }: ContactFormProps): JSX.Element => {
               autoComplete="email"
               placeholder="email"
               required
-               className="col-span-3 md:col-span-2  text-gray-900 bg-gray-300 py-2 px-2"
+              className="col-span-3 md:col-span-2  text-gray-900 bg-gray-300 py-2 px-2 mb-3 md:mb-0"
               value={email}
               onChange={(e)=>setEmail(e.target.value)}
               />
           </div>
           <div className="phone grid grid-cols-3 align-middle items-center border-b-2 border-gray-500">
-            <label htmlFor="frm-phone" className="col-span-3 md:col-span-1  text-center w-full  md:text-left border-2 border-gray-200  bg-gray-800 mt-3 pl-1 md:pl-4 pr-1 md:pr-3 md:border-0 md:bg-logohovercolor">Phone</label>
+            <label htmlFor="frm-phone" className="col-span-3 md:col-span-1 text-gray-900  text-center font-bold py-2 w-full  md:text-left border-2 border-gray-200  bg-logocolor md:m-3 pl-1 md:pl-4 pr-1 md:pr-3 md:border-0 md:bg-logocolor">Phone</label>
             <input 
               type="phone" 
               id="frm-phone" 
@@ -90,13 +90,13 @@ const ContactForm = ({ slice }: ContactFormProps): JSX.Element => {
               autoComplete="phone"
               placeholder="phone"
               required
-              className="col-span-3 md:col-span-2  text-gray-900 bg-gray-300 py-2 px-2"
+              className="col-span-3 md:col-span-2  text-gray-900 bg-gray-300 py-2 px-2 mb-3 md:mb-0"
               value={phone}
               onChange={(e)=>setPhone(e.target.value)}
               />
           </div>
           <div className="first-name grid grid-cols-3 align-middle items-center border-b-2 border-gray-500">
-            <label htmlFor="frm-first-name" className="col-span-3 md:col-span-1  text-center w-full  md:text-left border-2 border-gray-200 bg-gray-800 mt-3 pl-1 md:pl-4 pr-1 md:pr-3 md:border-0 md:bg-logohovercolor">First Name </label>
+            <label htmlFor="frm-first-name" className="col-span-3 md:col-span-1 text-gray-900 text-center font-bold py-2 w-full  md:text-left border-2 border-gray-200 bg-logocolor md:m-3 pl-1 md:pl-4 pr-1 md:pr-3 md:border-0 md:bg-logocolor">First Name </label>
             <input 
               type="first-name" 
               id="frm-first-name" 
@@ -104,13 +104,13 @@ const ContactForm = ({ slice }: ContactFormProps): JSX.Element => {
               autoComplete="first-name"
               placeholder="first-name"
               required
-              className="col-span-3 md:col-span-2  text-gray-900 bg-gray-300 py-2 px-2"
+              className="col-span-3 md:col-span-2  text-gray-900 bg-gray-300 py-2 px-2 mb-3 md:mb-0"
               value={firstName}
               onChange={(e)=>setFirstName(e.target.value)}
               />
           </div>
-          <div className="last-name grid grid-cols-3 align-middle items-center border-b-2 border-gray-500">
-            <label htmlFor="frm-last-name" className="col-span-3 md:col-span-1  text-center w-full  md:text-left border-2 border-gray-200  bg-gray-800 mt-3  pl-1 md:pl-4 pr-1 md:pr-3 md:border-0 md:bg-logohovercolor">Last Name</label>
+          <div className="last-name grid grid-cols-3 align-middle items-center border-b-2 mb-2 border-gray-500">
+            <label htmlFor="frm-last-name" className="col-span-3 md:col-span-1 text-gray-900 text-center font-bold py-2 w-full  md:text-left border-2 border-gray-200  bg-logocolor md:m-3  pl-1 md:pl-4 pr-1 md:pr-3 md:border-0 md:bg-logocolor">Last Name</label>
             <input 
               type="last-name" 
               id="frm-last-name" 
@@ -118,17 +118,17 @@ const ContactForm = ({ slice }: ContactFormProps): JSX.Element => {
               autoComplete="last-name"
               placeholder="last-name"
               required
-              className="col-span-3 md:col-span-2   text-gray-900 bg-gray-300 py-2 px-2 "
+              className="col-span-3 md:col-span-2   text-gray-900 bg-gray-300 py-2 px-2 mb-3 md:mb-0"
               value={lastName}
               onChange={(e)=>setLastName(e.target.value)}
               />
           </div>
       <div className="last-name grid grid-cols-3 align-middle items-center">
-        <label htmlFor="frm-message" className="col-span-3  pl-1 md:pl-4 pr-1 md:pr-3 md:col-span-1  text-center w-full  md:text-left border-2 border-gray-200  bg-gray-800 mt-3 md:border-0 md:bg-logohovercolor">Message</label>
+        <label htmlFor="frm-message" className="col-span-3  pl-1 pt-3 md:pt-0 md:pl-4 h-full pr-1 md:pr-3 md:col-span-1 text-gray-900 font-bold py-2 text-center w-full  md:text-left border-2 border-gray-200  bg-logocolor md:m-3 md:border-0 md:bg-logocolor">Message</label>
         <textarea id="frm-message" className="col-span-3 md:col-span-2  text-gray-900 bg-gray-300 py-2 px-2" rows={6} name="message" onChange={(e)=>setMessage(e.target.value)} value={message}/>
       </div>
       <div className="button flex w-full">
-        <button type="submit" className="w-full border-2 border-gray-200  py-4 bg-gray-400 hover:bg-gray-600 mt-4">Submit</button>
+        <button type="submit" className="w-full border-2 border-gray-200 md:ml-3 py-4 bg-gray-400 hover:bg-gray-600 mt-4">Submit</button>
       </div>
 
         </form>
